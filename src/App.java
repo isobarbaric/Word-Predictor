6dc039630fee1bc7460624d70331abf069bd6945
isobarbaric <shahkrish2016@gmail.com>
2022-03-24 18:40:35 -0400

    pog fixed indexing bug

--- src/App.java
+++ src/App.java
@@ -12,24 +12,25 @@ public class App {
         Scanner scanner = new Scanner(new File("src/simple-dictionary.txt"));
         Trie trie = new Trie();
         ArrayList<String> words = new ArrayList<String>();
-        while (scanner.hasNextLine()) {
-            try {
-                String currentWord = scanner.nextLine();
-                // print("'" + currentWord + "'");
-                words.add(currentWord.toLowerCase());
-                trie.insert(currentWord.toLowerCase());    
-            } catch (NullPointerException e) {
-                // seems to be getting stuck at capital words 
-                e.printStackTrace();
-            }
-        }
-        print(trie.toString());
-        // trie.insert("monke");
-        // print(Boolean.toString(trie.search("wound 1")));
+        // while (scanner.hasNextLine()) {
+        //     String currentWord = scanner.nextLine();
+        //     words.add(currentWord);
+        //     trie.insert(currentWord);    
+        // }
+        
+        print(Boolean.toString(trie.search("calle")));
+        // print(trie.toString());
+        // print(Boolean.toString(trie.search("p")));
+        // print(Boolean.toString(trie.search("monke")));
+        // boolean visited[] = new boolean[trie.nodeIDCounter];
+        // for (int i = 0; i < visited.length; i++) visited[i] = false;
+        // trie.DepthFirstSearch("A", 26, visited);
+        // trie.insert("chicken");
+        // print(trie.toString());
+        // print(Boolean.toString(trie.search("monke")));
         // print("Successfully added all words.");
         // for (int i = 0; i < words.size(); i++) 
-            // System.out.println("'" + words.get(i) + "'");
-            // System.out.println(words.get(i) + " " + Boolean.toString(trie.search(words.get(i).strip())));
+        //     System.out.println(words.get(i) + " " + Boolean.toString(trie.search(words.get(i).strip())));
         // print("Searching for words:");
         // print(Boolean.toString(trie.search("warmth")));
         // ArrayList<String> monke = trie.possibleWords("trans");
--- src/Trie.java
+++ src/Trie.java
@@ -6,54 +6,67 @@ public class Trie {
     
     private HashMap<Character, Node> parentNodes = new HashMap<Character, Node>();
     private ArrayList<Node> graph = new ArrayList<Node>();
-    private static int nodeIDCounter = 0;
+    static int nodeIDCounter = 0;
 
     public class Node {
         HashMap<Character, Node> children = new HashMap<Character, Node>();
+        char identity;
         int nodeID = -1;
         boolean wordOver = false;
-        Node() {
+        Node(char identity) {
             nodeID = nodeIDCounter;
             graph.add(this);
             nodeIDCounter++;
+            this.identity = identity;
         }
-        public void add(Character newChild) {
+        public void add(char newChild) {
             if (children.containsKey(newChild)) return;
-            children.put(newChild, new Node());
+            children.put(newChild, new Node(newChild));
             graph.set(nodeID, this);
         }
     }   
 
     public void insert(String wordToAdd) {
         int previousNodeIndex = parentNodes.get(wordToAdd.charAt(0)).nodeID; 
+        String checker = "";
+        checker += graph.get(previousNodeIndex).identity;
         for (int i = 1; i < wordToAdd.length(); i++) {
-            if (!graph.get(previousNodeIndex).children.containsKey(wordToAdd.charAt(i))) {
+            if (!graph.get(previousNodeIndex).children.containsKey(wordToAdd.charAt(i))) 
                 graph.get(previousNodeIndex).add(wordToAdd.charAt(i));
-            }
             previousNodeIndex = graph.get(previousNodeIndex).children.get(wordToAdd.charAt(i)).nodeID;
+            checker += graph.get(previousNodeIndex).identity;
         }
+        assert(checker.equals(wordToAdd));
         graph.get(previousNodeIndex).wordOver = true;
     }
 
     public boolean search(String wordToSearchFor) {
         int currentNodeIndex = parentNodes.get(wordToSearchFor.charAt(0)).nodeID;
+        String checker = "";
+        checker += graph.get(currentNodeIndex).identity;
         for (int i = 1; i < wordToSearchFor.length(); i++) {
-            if (!graph.get(currentNodeIndex).children.containsKey(wordToSearchFor.charAt(i))) return false;
+            if (!graph.get(currentNodeIndex).children.containsKey(wordToSearchFor.charAt(i))) {
+                System.out.println(graph.get(currentNodeIndex).identity);
+                return false;
+            }
             currentNodeIndex = graph.get(currentNodeIndex).children.get(wordToSearchFor.charAt(i)).nodeID;
+            checker += graph.get(currentNodeIndex).identity;
         }
+        assert(checker.equals(wordToSearchFor));
         return true;
     }
 
     public ArrayList<String> possibleWords(String prefix) {
         ArrayList<String> listWords = new ArrayList<String>();
-        int lastCharacterNodeIndex = parentNodes.get(prefix.charAt(0)).nodeID;
+        int lastcharNodeIndex = parentNodes.get(prefix.charAt(0)).nodeID;
         for (int i = 1; i < prefix.length(); i++) {
-            if (!graph.get(lastCharacterNodeIndex).children.containsKey(prefix.charAt(i))) return listWords;
-            lastCharacterNodeIndex = graph.get(lastCharacterNodeIndex).children.get(prefix.charAt(i)).nodeID;
+            if (!graph.get(lastcharNodeIndex).children.containsKey(prefix.charAt(i))) 
+                return listWords;
+            lastcharNodeIndex = graph.get(lastcharNodeIndex).children.get(prefix.charAt(i)).nodeID;
         }
         boolean visited[] = new boolean[nodeIDCounter];
         for (int i = 0; i < visited.length; i++) visited[i] = false;
-        listWords = DepthFirstSearch(prefix, lastCharacterNodeIndex, visited);
+        listWords = DepthFirstSearch(prefix, lastcharNodeIndex, visited);
         return listWords;
     }
 
@@ -61,11 +74,9 @@ public class Trie {
         ArrayList<String> allWords = new ArrayList<String>();
         if (visited[currentNodeIndex]) return allWords;
         visited[currentNodeIndex] = true;
-        if (graph.get(currentNodeIndex).wordOver) {
-            System.out.println(currentString);
+        if (graph.get(currentNodeIndex).wordOver) 
             allWords.add(currentString);
-        }
-        for (Character letter : graph.get(currentNodeIndex).children.keySet())
+        for (char letter : graph.get(currentNodeIndex).children.keySet())
             allWords.addAll(DepthFirstSearch(currentString + letter, graph.get(currentNodeIndex).children.get(letter).nodeID, visited));
         return allWords;
     } 
@@ -76,17 +87,17 @@ public class Trie {
         ArrayList<String> allWords = new ArrayList<String>();
         boolean visited[] = new boolean[nodeIDCounter];
         for (int i = 0; i < visited.length; i++) visited[i] = false;
-        for (Character letter: parentNodes.keySet())
-            allWords.addAll(DepthFirstSearch(letter.toString(), parentNodes.get(letter).nodeID, visited));
+        for (char letter: parentNodes.keySet())
+            allWords.addAll(DepthFirstSearch(Character.toString(letter), parentNodes.get(letter).nodeID, visited));
         for (String word: allWords) 
-            toDisplay += word + '\n';
+            toDisplay += word + ", ";
         return toDisplay;
-    }
+    }   
 
     Trie() {
-        for (char letter = 'a'; letter <= 'z'; letter++) {
-            parentNodes.put(letter, new Node());
-            graph.add(parentNodes.get(letter));
-        }
+        for (char letter = 'a'; letter <= 'z'; letter++) 
+            parentNodes.put(letter, new Node(letter));
+        for (char letter = 'A'; letter <= 'Z'; letter++) 
+            parentNodes.put(letter, new Node(letter));
     }
 }
